6fa9e43da232684e2445a54cf0f6464efec6e0ee
Ivo Zubovic <ivo.zubovic@gmail.com>
2021-09-06 14:58:17 +0200

    Updated OpenCV library with linked ffmpeg codecs for Linux

--- VideoEditor/VideoEditor.pro
+++ VideoEditor/VideoEditor.pro
@@ -76,3 +76,5 @@ win32-g++:CONFIG(release, debug|release): PRE_TARGETDEPS += $$PWD/ThirdParty/lib
 else:win32-g++:CONFIG(debug, debug|release): PRE_TARGETDEPS += $$PWD/ThirdParty/lib/libopencv_videoio453d.a
 else:win32:!win32-g++:CONFIG(release, debug|release): PRE_TARGETDEPS += $$PWD/ThirdParty/lib/opencv_videoio453.lib
 else:win32:!win32-g++:CONFIG(debug, debug|release): PRE_TARGETDEPS += $$PWD/ThirdParty/lib/opencv_videoio453d.lib
+
+unix:!macx: LIBS += -L$$PWD/ThirdParty/lib/ -lopencv_videoio
--- VideoEditor/VideoProcessor/VideoThumbnailProvider.cpp
+++ VideoEditor/VideoProcessor/VideoThumbnailProvider.cpp
@@ -4,7 +4,8 @@ cv::Mat VideoThumbnailProvider::getThumbnail(std::string const& videoPath)
 {
     cv::VideoCapture videoCapture;
 
-	videoCapture.open(videoPath);
+    char const * p = videoPath.c_str();
+    videoCapture.open(p);
 
 	bool const captureOpen = videoCapture.isOpened();
 
@@ -12,15 +13,14 @@ cv::Mat VideoThumbnailProvider::getThumbnail(std::string const& videoPath)
         return cv::Mat(100, 100, CV_8UC4);
     }
 
-	int const frameCount{ static_cast<int>(videoCapture.get(cv::CAP_PROP_FRAME_COUNT)) };
+    int const frameCount{ static_cast<int>(videoCapture.get(cv::CAP_PROP_FRAME_COUNT)) };
 
 	cv::Mat thumbnail;
+    int const frameIndex = (frameCount > 5000) ? 5000 : frameCount / 2;
 
-	for (int i = 0; i < frameCount / 2; ++i) {
+    for (int i = 0; i < frameIndex; ++i) {
 		videoCapture >> thumbnail;
 	}
-
-	cv::rotate(thumbnail, thumbnail, cv::ROTATE_90_COUNTERCLOCKWISE);
 
 	return thumbnail;
 }
